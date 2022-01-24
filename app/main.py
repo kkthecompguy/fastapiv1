13aabbb18e7a66bf19fec4ee32b7db43895a4e53
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-01-24 21:21:49 +0300

    added profile heroku file

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routes import posts, users, auth, votes
@@ -6,8 +7,19 @@ from .routes import posts, users, auth, votes
 app = FastAPI()
 
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
+origins = [
+  "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
